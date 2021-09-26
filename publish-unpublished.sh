2422da6abc0b75c40d43f6126a75460e82e902e3
Timofey <timoxa17@gmail.com>
2021-09-26 20:39:07 +0300

    publish script for monorepo

---
+++ publish-unpublished.sh
@@ -0,0 +1,15 @@
+workspacesPath=$(npm pkg get workspaces.0);
+workspacesPath=${workspacesPath:1:-1};
+for ws in $(realpath $(dirname $0))/${workspacesPath}; do
+	cd ${ws};
+	name=$(npm pkg get name);
+	name=${name:1:-1}; # remove quotes
+	version=$(npm pkg get version);
+	version=${version:1:-1}; # remove quotes
+	if [[ $(npm view ${name}@${version}) ]]; then
+		echo "${name}@${version} is already published, skip";
+	else
+		echo "${name}@${version} is not published yet, let's do it";
+		npm publish;
+	fi;
+done;
