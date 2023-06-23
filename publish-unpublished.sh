2dad6ea79a56dc9d9bde605c8e539d98e95dd7d5
Timofey Tiunov <timoxa17@gmail.com>
2023-06-23 13:56:22 +0300

    fix publish job

--- publish-unpublished.sh
+++ publish-unpublished.sh
@@ -1,10 +1,10 @@
-workspacesPath=$(npm pkg get workspaces.0);
+workspacesPath=$(npm pkg get workspaces.0 | head -n 1);
 workspacesPath=${workspacesPath:1:-1};
 for ws in $(realpath $(dirname $0))/${workspacesPath}; do
 	cd ${ws};
-	name=$(npm pkg get name);
+	name=$(npm pkg get name | head -n 1);
 	name=${name:1:-1}; # remove quotes
-	version=$(npm pkg get version);
+	version=$(npm pkg get version | head -n 1);
 	version=${version:1:-1}; # remove quotes
 	if [[ $(npm view ${name}@${version}) ]]; then
 		echo "${name}@${version} is already published, skip";
